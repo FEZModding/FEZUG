b3e7bba915528049c43818f2e839fe68eea47868
Krzyhau <krzyht@gmail.com>
2023-01-13 22:03:40 +0100

    add timescale feature

---
+++ Features/Timescaler.cs
@@ -0,0 +1,56 @@
+﻿using FezEngine.Services;
+using FezEngine.Tools;
+using FEZUG.Features.Console;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FEZUG.Features
+{
+    internal class Timescaler : IFezugCommand
+    {
+        public static float Timescale = 1.0f;
+
+        public string Name => "timescale";
+
+        public string HelpText => "timescale <value> - changes game's simulation scale";
+
+        [ServiceDependency]
+        public ISoundManager SoundManager { private get; set; }
+
+        public List<string> Autocomplete(string[] args)
+        {
+            string timescaleStr = Timescale.ToString("0.000", CultureInfo.InvariantCulture);
+            if (timescaleStr.StartsWith(args[0])) return new List<string> { timescaleStr };
+            return null;
+        }
+
+        public bool Execute(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                FezugConsole.Print($"Incorrect number of parameters: '{args.Length}'", FezugConsole.OutputType.Warning);
+                return false;
+            }
+
+            if (!float.TryParse(args[0], NumberStyles.Number, CultureInfo.InvariantCulture, out float timescale))
+            {
+                FezugConsole.Print($"Incorrect timescale: '{args[0]}'", FezugConsole.OutputType.Warning);
+                return false;
+            }
+
+            Timescale = timescale;
+
+            FezugConsole.Print($"Timescale has been set to {Timescale.ToString("0.000", CultureInfo.InvariantCulture)}");
+
+            return true;
+        }
+    }
+}
--- Patches/Fez.cs
+++ Patches/Fez.cs
@@ -4,11 +4,16 @@ using FezGame.Components;
 using FEZUG;
 using FEZUG.Features;
 using Microsoft.Xna.Framework;
+using System;
+using System.Diagnostics;
 
 namespace FezGame
 {
     public class patch_Fez : Fez
     {
+        private double timescaledGameTime;
+        private double timescaledElapsedTime;
+
         public Fezug Fezug;
 
         protected extern void orig_Initialize();
@@ -21,6 +26,33 @@ namespace FezGame
             ServiceHelper.AddComponent(Fezug = new Fezug(this));
             ServiceHelper.AddComponent(Fezug.Rendering);
             Logger.Log("FEZUG", "FEZUG initialized!");
+
+            timescaledGameTime = 0.0f;
+        }
+
+        protected extern void orig_Update(GameTime gameTime);
+        protected override void Update(GameTime gameTime)
+        {
+            if(gameTime.TotalGameTime.Ticks == 0)
+            {
+                timescaledGameTime = 0.0f;
+            }
+            timescaledElapsedTime = gameTime.ElapsedGameTime.TotalSeconds * Timescaler.Timescale;
+            timescaledGameTime += timescaledElapsedTime;
+
+            orig_Update(new GameTime(
+                TimeSpan.FromSeconds(timescaledGameTime),
+                TimeSpan.FromSeconds(timescaledElapsedTime)
+            ));
+        }
+
+        protected extern void orig_Draw(GameTime gameTime);
+        protected override void Draw(GameTime gameTime)
+        {
+            orig_Draw(new GameTime(
+                TimeSpan.FromSeconds(timescaledGameTime),
+                TimeSpan.FromSeconds(timescaledElapsedTime)
+            ));
         }
     }
 }
