8e195981c65a1daca0cca9b6ee016cd0b4d0532f
Krzyhau <krzyht@gmail.com>
2023-02-26 15:04:38 +0100

    implement hud-related console variables

--- Features/TextHud.cs
+++ Features/TextHud.cs
@@ -1,5 +1,6 @@
 ﻿using FezEngine.Tools;
 using FezGame.Services;
+using FEZUG.Features.Console;
 using FEZUG.Helpers;
 using Microsoft.Xna.Framework;
 using System;
@@ -13,6 +14,14 @@ namespace FEZUG.Features
 {
     public class TextHud : IFezugFeature
     {
+        private FezugVariable hud_level;
+        private FezugVariable hud_position;
+        private FezugVariable hud_velocity;
+        private FezugVariable hud_state;
+        private FezugVariable hud_viewpoint;
+
+        private float lastWidth;
+        private TimeSpan lastWidthUpdateTime;
 
         [ServiceDependency]
         public IPlayerManager PlayerManager { private get; set; }
@@ -25,7 +34,21 @@ namespace FEZUG.Features
 
         public void Initialize()
         {
-
+            Func<string, string, FezugVariable> CreateHudVariable = delegate (string name, string desc)
+            {
+                return new FezugVariable(name, $"If set, enables {desc} text hud.", "0")
+                {
+                    SaveOnChange = true,
+                    Min = 0,
+                    Max = 1
+                };
+            };
+            
+            hud_level = CreateHudVariable("hud_level", "level");
+            hud_position = CreateHudVariable("hud_position", "Gomez's position");
+            hud_velocity = CreateHudVariable("hud_velocity", "Gomez's velocity");
+            hud_state = CreateHudVariable("hud_state", "Gomez's state");
+            hud_viewpoint = CreateHudVariable("hud_viewpoint", "camera viewpoint");
         }
 
         private void DrawText(string text, Vector2 pos)
@@ -42,47 +65,81 @@ namespace FEZUG.Features
 
         public void DrawHUD(GameTime gameTime)
         {
-            int width = (int)DrawingTools.DefaultFont.MeasureString("Position: (X:-999.999 Y:-999.999 Z:-999.999)").X * 2;
+            var linesToDraw = new List<string>()
+            {
+                $"FEZUG {Fezug.Version}"
+            };
 
-            DrawingTools.DrawRect(new Rectangle(2, 2, width, 200), new Color(0, 0, 0, 220));
+
+            if (hud_level.ValueBool)
+            {
+                linesToDraw.Add($"Level: {LevelManager.Name}");
+            }
+
+            if (hud_position.ValueBool)
+            {
+                string posX = PlayerManager.Position.X.ToString("0.000", CultureInfo.InvariantCulture);
+                string posY = PlayerManager.Position.Y.ToString("0.000", CultureInfo.InvariantCulture);
+                string posZ = PlayerManager.Position.Z.ToString("0.000", CultureInfo.InvariantCulture);
+
+                linesToDraw.Add($"Position: (X:{posX} Y:{posY} Z:{posZ})");
+            }
+
+            if (hud_velocity.ValueBool)
+            {
+                string velX = PlayerManager.Velocity.X.ToString("0.000", CultureInfo.InvariantCulture);
+                string velY = PlayerManager.Velocity.Y.ToString("0.000", CultureInfo.InvariantCulture);
+                string velZ = PlayerManager.Velocity.Z.ToString("0.000", CultureInfo.InvariantCulture);
+
+                linesToDraw.Add($"Velocity: (X:{velX} Y:{velY} Z:{velZ})");
+            }
+
+            if (hud_state.ValueBool)
+            {
+                linesToDraw.Add($"State: {PlayerManager.Action}");
+            }
+
+            if (hud_viewpoint.ValueBool)
+            {
+                string Viewpoint = "";
+                switch (CameraManager.Viewpoint)
+                {
+                    case FezEngine.Viewpoint.Back:
+                        Viewpoint = "Back";
+                        break;
+                    case FezEngine.Viewpoint.Left:
+                        Viewpoint = "Left";
+                        break;
+                    case FezEngine.Viewpoint.Right:
+                        Viewpoint = "Right";
+                        break;
+                    case FezEngine.Viewpoint.Front:
+                        Viewpoint = "Front";
+                        break;
+                    default:
+                        Viewpoint = "Other";
+                        break;
+                }
+                linesToDraw.Add($"Viewpoint: {Viewpoint}");
+            }
+
+            float maxWidth = linesToDraw.Select(str => DrawingTools.DefaultFont.MeasureString(str).X * 2).Max();
+            if(maxWidth > lastWidth || (gameTime.TotalGameTime - lastWidthUpdateTime).TotalSeconds > 1.0f)
+            {
+                lastWidthUpdateTime = gameTime.TotalGameTime;
+                lastWidth = maxWidth;
+            }
 
             float padX = 10.0f;
-            DrawText($"FEZUG {Fezug.Version}", new Vector2(padX, 0.0f));
-            DrawText($"FEZUG {Fezug.Version}", new Vector2(padX, -1.0f));
-            DrawText($"Level: {LevelManager.Name}", new Vector2(padX, 30.0f));
+            DrawingTools.DrawRect(new Rectangle(2, 2, (int)(lastWidth + padX * 2), linesToDraw.Count * 30 + 20), new Color(0, 0, 0, 220));
 
-            string posX = PlayerManager.Position.X.ToString("0.000", CultureInfo.InvariantCulture);
-            string posY = PlayerManager.Position.Y.ToString("0.000", CultureInfo.InvariantCulture);
-            string posZ = PlayerManager.Position.Z.ToString("0.000", CultureInfo.InvariantCulture);
-
-            string velX = PlayerManager.Velocity.X.ToString("0.000", CultureInfo.InvariantCulture);
-            string velY = PlayerManager.Velocity.Y.ToString("0.000", CultureInfo.InvariantCulture);
-            string velZ = PlayerManager.Velocity.Z.ToString("0.000", CultureInfo.InvariantCulture);
-
-            DrawText($"Position: (X:{posX} Y:{posY} Z:{posZ})", new Vector2(padX, 60.0f));
-            DrawText($"Velocity: (X:{velX} Y:{velY} Z:{velZ})", new Vector2(padX, 90.0f));
-            DrawText($"State: {PlayerManager.Action}", new Vector2(padX, 120.0f));
-
-            string Viewpoint = "";
-            switch(CameraManager.Viewpoint)
+            for(int i = 0; i < linesToDraw.Count; i++)
             {
-                case FezEngine.Viewpoint.Back:
-                    Viewpoint = "Back";
-                    break;
-                case FezEngine.Viewpoint.Left:
-                    Viewpoint = "Left";
-                    break;
-                case FezEngine.Viewpoint.Right:
-                    Viewpoint = "Right";
-                    break;
-                case FezEngine.Viewpoint.Front:
-                    Viewpoint = "Front";
-                    break;
-                default:
-                    Viewpoint = "Other";
-                    break;
+                var line = linesToDraw[i];
+                DrawText(line, new Vector2(padX, i * 30.0f));
+                if (i == 0) DrawText(line, new Vector2(padX, (i*30.0f)-1.0f));
             }
-            DrawText($"Viewpoint: {Viewpoint}", new Vector2(padX, 150.0f));
+
         }
     }
 }
