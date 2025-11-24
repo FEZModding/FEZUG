fdb254922eb833a2ccc478ba0f44d16e71969013
Jenna Sloan <jenna3715@gmail.com>
2025-11-23 23:27:28 -0600

    added collision type labels to TrileCollisionsDraw
    also added an option to not draw the hitboxes for the "None" collision type

--- Features/TrileCollisionsDraw.cs
+++ Features/TrileCollisionsDraw.cs
@@ -1,21 +1,18 @@
 ﻿using FezEngine;
+using FezEngine.Components;
 using FezEngine.Effects;
-using FezEngine.Services;
 using FezEngine.Structure;
 using FezEngine.Tools;
-using FezGame.Services;
-using FEZUG.Features.Console;
 using FEZUG.Helpers;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System.Linq;
-using System.Security.Cryptography;
 
 namespace FEZUG.Features
 {
     internal class TrileCollisionsDraw : WireframeDraw
     {
         public static TrileCollisionsDraw Instance;
+        private bool ShowNone = true;
 
         public TrileCollisionsDraw() : base()
         {
@@ -32,17 +29,54 @@ namespace FEZUG.Features
 
         private readonly Dictionary<TrileEmplacement, Dictionary<FaceOrientation, CollisionType>> invisibleTriles = [];
         private readonly Dictionary<CollisionType, Mesh> CollisionMeshes = [];
-        private readonly Dictionary<FaceOrientation, Quaternion> FaceOrientationToQuaternion = ((IEnumerable<FaceOrientation>)Enum.GetValues(typeof(FaceOrientation))).Select(face => {
-            return (key: face, value: Quaternion.CreateFromAxisAngle(face.AsVector(), 0));
-        }).ToDictionary(p => p.key, p => p.value);
 
         protected override Mesh[] RefreshBoundingBoxMeshs()
         {
-            var i = 0;
-            foreach (var color in colors)
+            var sortedFaces = Common.Util.GetValues<FaceOrientation>().OrderBy(a => (int)a);
+            foreach (var color in colors.OrderBy(a => (int)a.Key))
             {
-                CollisionMeshes[color.Key] = CreateHitboxMesh(color.Value);
+                var mesh = CollisionMeshes[color.Key] = new Mesh();
+                mesh.Effect = new DefaultEffect.Textured();
 
+                Waiters.Wait(() =>
+                {
+                    return DrawingTools.DefaultFont != null && DrawingTools.DefaultFontSize > 0f && DrawingTools.GraphicsDevice != null && DrawingTools.Batch != null;
+                }, () =>
+                {
+                    string text = color.Key.ToString();
+                    Vector2 textSize = DrawingTools.DefaultFont.MeasureString(text) * DrawingTools.DefaultFontSize;
+                    var BackgroundColor = color.Value;
+
+                    BackgroundColor.A = 200;
+                    RenderTarget2D textTexture = new(DrawingTools.GraphicsDevice, (int)textSize.X, (int)textSize.Y, mipMap: false, DrawingTools.GraphicsDevice.PresentationParameters.BackBufferFormat, DrawingTools.GraphicsDevice.PresentationParameters.DepthStencilFormat, 0, RenderTargetUsage.PreserveContents);
+
+                    DrawingTools.GraphicsDevice.SetRenderTarget(textTexture);
+                    DrawingTools.GraphicsDevice.PrepareDraw();
+                    DrawingTools.GraphicsDevice.Clear(ClearOptions.Target, BackgroundColor, 1f, 0);
+                    if (Culture.IsCJK)
+                    {
+                        DrawingTools.Batch.BeginLinear();
+                    }
+                    else
+                    {
+                        DrawingTools.Batch.BeginPoint();
+                    }
+                    DrawingTools.DrawText(text, Vector2.Zero, 0, 1f, Color.Black);
+                    DrawingTools.DrawText(text, Vector2.Zero + Vector2.One, 0, 1f, Color.White);
+                    DrawingTools.EndBatch();
+                    DrawingTools.GraphicsDevice.SetRenderTarget(null);
+
+                    mesh.Texture = textTexture;
+                    mesh.SamplerState = Culture.IsCJK ? SamplerState.AnisotropicClamp : SamplerState.PointClamp;
+                    mesh.Material.Opacity = 1;
+                    mesh.Texture = textTexture;
+                    mesh.AlwaysOnTop = true;
+
+                    foreach (FaceOrientation orientation in sortedFaces)
+                    {
+                        Group newGroup = mesh.AddFace(Vector3.One, Vector3.Zero, orientation, true);
+                    }
+                });
             }
             return [.. CollisionMeshes.Values];
         }
@@ -67,31 +101,22 @@ namespace FEZUG.Features
             foreach (var trile in LevelManager.Triles)
             {
                 Dictionary<FaceOrientation, CollisionType> faces = trile.Value.Trile.Faces;
-            }
-            foreach (var pair in invisibleTriles)
-            {
-                var trilePos = pair.Key;
-                var type = invisibleTriles[trilePos];
-                if (pair.Value.TryGetValue(cameraViewpoint, out var collisionType))
+                foreach (var face in faces)
                 {
-                    var trilebb = CollisionMeshes[collisionType];
-                    if (LevelManager.Triles.TryGetValue(trilePos, out var trilePosActual))
+                    if(face.Value == CollisionType.None && !ShowNone)
                     {
-                        trilebb.Position = LevelManager.Triles[trilePos].Position + Vector3.One * 0.5f;
+                        continue;
                     }
-                    else
+                    if (CollisionMeshes.TryGetValue(face.Value, out Mesh mesh))
                     {
-                        trilebb.Position = trilePos.AsVector + Vector3.One * 0.5f;
-                    }
-                    trilebb.Draw();
-                }
-                else
-                {
-                    foreach (var faces in pair.Value)
-                    {
-                        var trilebb = CollisionMeshes[faces.Value];
-                        trilebb.Position = trilePos.AsVector + Vector3.One * 0.5f;
-                        trilebb.Draw();
+                        foreach (var group in mesh.Groups)
+                        {
+                            group.Enabled = false;
+                        }
+                        Group activeGroup = mesh.Groups[(int)face.Key];
+                        activeGroup.Position = trile.Value.Position + Vector3.One * 0.5f + Vector3.One * 0.5f * face.Key.AsVector();
+                        activeGroup.Enabled = true;
+                        mesh.Draw();
                     }
                 }
             }
@@ -113,6 +138,16 @@ namespace FEZUG.Features
                 set => Instance.WireframesEnabled = value;
             }
             public override Mesh[] BoundingBoxes => Instance.BoundingBoxes;
+
+            protected override Dictionary<string, Func<string[], string>> AdditionalChoices => new()
+            {
+                {
+                    "toggle_hide_none", (args)=>{
+                        Instance.ShowNone = !Instance.ShowNone;
+                        return $"Wireframes for the \"None\" CollisionType have been {(Instance.ShowNone ? "enabled" : "disabled")}.";
+                    }
+                } 
+            };
         }
     }
 }
